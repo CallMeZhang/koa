93d5930e4ae20ac389e953cce61999ca8480f7d5
张和 <zhanghe0@126.com>
2018-12-11 14:51:49 +0800

    first commit

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router = require('koa-router')()
 let getClientIp= require('./utils')
 router.get('/', async (ctx, next) => {
+	// console.log(ctx)
   await ctx.render('index', {
     title: 'Hello Koa 2!',
     ip:getClientIp(ctx.req)
@@ -9,12 +10,12 @@ router.get('/', async (ctx, next) => {
 
 router.get('/users', async (ctx, next) => {
 	await ctx.render('users', {
-		title: 'Hello Koa user',
+		title: '访问用户列表',
 		tableData:[
 		  {
 			ip: '2016-05-02',
 			address: '上海市普陀区金沙江路 1518 弄'
-		}]
+		}],
 	})
 })
 
